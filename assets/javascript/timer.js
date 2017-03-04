b89cee525806da2a9d07934e6dfd61556a70e1d1
Sandy Seay <seaysandra@bellsouth.net>
2017-03-04 02:30:15 -0500

    added new js

--- assets/javascript/timer.js
+++ assets/javascript/timer.js
@@ -8,31 +8,38 @@
   var wrongAnswers = [];
   var noAnswer = [];
   var unansweredQuestions = [];
-  var remainingQuestions = [ {
-    
-    q: "what is the color of the moon?",
-    answer: ["Gray", "Blue", "Red", "Green"],
+  var remainingQuestions = {}
+  
+  $("#maingame, #endScreen").hide();
+  $('#A, #B, #C').click(answerCheck);
+  $("#StartButton").click(init);
+  
+  
+  var questions = [{  
+    question: "SuperBowl XXVII- Pasadena, California  Dallas Cowboys 52, Buffalo Bills 17 The 1993 half-time show ranks as one of the all time best making marching bands a thing of the past! Who performed changing the future of the SuperBowl half-time show?",
+    choices: ["Michael Jackson", "Tony Bennett, Patti LaBelle, Miami Sound Machine", "Gloria Estefan, Dorothy Hammill and Brian Boitano", "Whitney Houston"],
     correct: "0"
-  }, {
-    q: "Who played in the Georgia Dome for the half-time show?",
-    answer: ["4", "7", "8", "Clint Black, Tanya Tucker, Travis Tritt, Naomi and Wynonna Judd"],
-    correct: "Clint Black, Tanya Tucker, Travis Tritt, Naomi and Wynonna Judd"
-  }, {
-    q: "Name the Superbowl 1999 half-time performer/s?",
-    answer: ["Boyz II Men, Smokey Robinson, Queen Latifah, Martha Reeves and The Temptations","Big Bad Voodoo Daddy, Stevie Wonder, and Gloria Estefan", "Cher", "KISS"],
-    correct: "Big Bad Voodoo Daddy, Stevie Wonder, and Gloria Estefan"
   },
-   {
-    q: "In 2000, the half-time show featured what superstar/s?",
-    answer: ["Phil Collins, Christina Aguilera, Enrique Iglesias, Tina Turner, Toni Braxton, and Edward James Olmos","Pink, Christine Aguilera, Faith Hill and Travis Tritt", "Madonna", "Britney, Aerosmith, NSYNC"],
-  	correct: "Phil Collins, Christina Aguilera, Enrique Iglesias, Tina Turner, Toni Braxton, and Edward James Olmos" 
+  {
+    question: "Who played in the Georgia Dome for the half-time show?",
+    choices: ["Bryan Adams, Rod Stewart and Sting","Tim McGraw, Faith Hill, Vince Gill", "Michael Jackson", "Kris Kross, TLC, and Michael Jackson", "Clint Black, Tanya Tucker, Travis Tritt, Naomi and Wynonna Judd"],
+    correct: "3"
+  },
+  {
+    question: "Name the Superbowl 1999 half-time performer/s?",
+    choices: ["Boyz II Men, Smokey Robinson, Queen Latifah, Martha Reeves and The Temptations","Big Bad Voodoo Daddy, Stevie Wonder, and Gloria Estefan", "Cher", "KISS"],
+    correct: "1"
+  },
+  {
+    question: "In 2000, the half-time show featured what superstar/s?",
+    choices: ["Phil Collins, Christina Aguilera, Enrique Iglesias, Tina Turner, Toni Braxton, and Edward James Olmos","Pink, Christine Aguilera, Faith Hill and Travis Tritt", "Madonna", "Britney, Aerosmith, NSYNC"],
+  	correct: "0" 
   },
 ];
 
-            function startGame() {
-            location.replace("https://www.w3schools.com")
+ function startGame() {
+    location.replace("superBowl_93.html")
               }
-
 
   function incrementTimer() {
     timer = setTimeout( function () {
@@ -52,13 +59,13 @@
 
   function startTimer() {
     clearTimeout( timer );
-    timeRemaining = 10; // in seconds
+    timeRemaining = 20; // in seconds
     incrementTimer();
   }
 
   function askQuestion() {
     if ( remainingQuestions.length <= 0 ) {
-      // you done
+      $( '#asked-question' ).html( questionObject.q );
       clearTimeout(timer);
       alert (" you got correct: " + rightAnswers.length);
       alert (" you got wrong: " + wrongAnswers.length);
